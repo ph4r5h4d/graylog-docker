702e68d41e62b8e91354e12942ff8e160893dfd6
Farshad Nematdoust <farshad@nematdoust.com>
2019-03-03 13:22:34 +0330

    Make entrypoint graylog address static

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -46,12 +46,12 @@ graylog() {
   "${JAVA_HOME}/bin/java" \
     ${GRAYLOG_SERVER_JAVA_OPTS} \
     -jar \
-    -Dlog4j.configurationFile="${GRAYLOG_HOME}/data/config/log4j2.xml" \
-    -Djava.library.path="${GRAYLOG_HOME}/lib/sigar/" \
+    -Dlog4j.configurationFile="/usr/share/graylog/data/config/log4j2.xml" \
+    -Djava.library.path="/usr/share/graylog/lib/sigar/" \
     -Dgraylog2.installation_source=docker \
-    "${GRAYLOG_HOME}/graylog.jar" \
+    "/usr/share/graylog/graylog.jar" \
     server \
-    -f "${GRAYLOG_HOME}/data/config/graylog.conf"
+    -f "/usr/share/graylog/data/config/graylog.conf"
 }
 
 run() {
